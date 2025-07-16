24ea3768471a7d4ff8ea27dc12d70d274fadc4c8
Manubothu Raghav <manubothuraghav@gmail.com>
2025-07-16 19:12:07 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://stickynotes-api.onrender.com',
+    baseUrl: 'https://stickynotes-api-rryo.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
